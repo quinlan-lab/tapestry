2803c9b5f80b49e0456b4fcea91657c7400907f4
petermchale <peter.thomas.mchale@gmail.com>
2025-09-24 07:45:00 -0600

    minor change

--- run-hiphase.sh
+++ run-hiphase.sh
@@ -4,7 +4,7 @@
 palladium_vcf_dir="/scratch/ucgd/lustre-labs/quinlan/data-shared/datasets/Palladium/TRGT/from_aws/GRCh38_v1.0_50bp_merge/493ef25/hiphase"
 palladium_bam_dir="/scratch/ucgd/lustre-labs/quinlan/data-shared/datasets/Palladium/hifi-bams/GRCh38"
 output_dir="/scratch/ucgd/lustre-labs/quinlan/data-shared/read-backed-phasing"
-reference_dir="/scratch/ucgd/lustre-labs/quinlan/data-shared/constraint-tools/reference/grch38" 
+reference="/scratch/ucgd/lustre-labs/quinlan/data-shared/constraint-tools/reference/grch38/hg38.analysisSet.fa.gz" 
 
 bin_dir="/uufs/chpc.utah.edu/common/HIPAA/u6018199/hiphase-v1.5.0-x86_64-unknown-linux-gnu" 
 export PATH=${bin_dir}:$PATH
@@ -26,7 +26,7 @@ for prefix in $prefixes; do
 		--output-vcf ${output_dir}/${prefix}.GRCh38.pbsv.phased.vcf.gz \
 		--bam ${palladium_bam_dir}/${prefix}.GRCh38.haplotagged.bam \
 		--output-bam ${output_dir}/${prefix}.GRCh38.haplotagged.bam \
-		--reference ${reference_dir}/hg38.analysisSet.fa.gz \
+		--reference ${reference} \
 		--summary-file ${output_dir}/${prefix}.GRCh38.hiphase.stats.tsv \
 		--blocks-file ${output_dir}/${prefix}.GRCh38.hiphase.blocks.tsv \
 		> ${output_dir}/${prefix}.log 2>&1 &
