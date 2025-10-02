3a84807911e0b97ca9a01876bed534d4995beb41
petermchale <peter.thomas.mchale@gmail.com>
2025-10-02 12:44:07 -0600

    expand to include unphased meth, where available

--- expand_to_all_cpgs.sh
+++ expand_to_all_cpgs.sh
@@ -5,6 +5,7 @@ pb_cpg_tool_mode='model' # mode of aligned_bam_to_cpg_scores
 # INPUT DIRS 
 reference="/scratch/ucgd/lustre-labs/quinlan/data-shared/constraint-tools/reference/grch38/hg38.analysisSet.fa" # /scratch/ucgd/lustre-labs/quinlan/u6018199/constraint-tools/download-process-data/download-reference-grch38.sh
 meth_founder_phased_dir="/scratch/ucgd/lustre-labs/quinlan/data-shared/dna-methylation/CEPH1463.GRCh38.hifi.${pb_cpg_tool_mode}.founder-phased" # output dir of phase_meth_to_founder_haps.py
+meth_read_phased_dir="/scratch/ucgd/lustre-labs/quinlan/data-shared/dna-methylation/CEPH1463.GRCh38.hifi.${pb_cpg_tool_mode}.read-backed-phased" # output dir of aligned_bam_to_cpg_scores
 
 # OUTPUT DIRS 
 output_dir="/scratch/ucgd/lustre-labs/quinlan/data-shared/dna-methylation/CEPH1463.GRCh38.hifi.${pb_cpg_tool_mode}.founder-phased.all-cpgs" 
@@ -25,22 +26,23 @@ python src/write_all_cpgs.py \
 prefixes=$(python src/util/get_palladium_prefixes.py)
 echo "Done loading prefixes..."
 
-# prefixes="200081" # TESTING 
+# prefixes="200081" 
 
 for prefix in $prefixes; do
     uid="${prefix}" # sample ID in joint-called multi-sample vcf
 
     # INPUT FILES 
-	bed_hap_map="${meth_founder_phased_dir}/${uid}.hap-map-blocks.bed" # bed file of hap-map blocks from src/phase_meth_to_founder_haps.py
 	bed_meth_founder_phased="${meth_founder_phased_dir}/${uid}.dna-methylation.founder-phased.bed" # bed file of founder-phased methylation levels from src/phase_meth_to_founder_haps.py
+	bed_meth_unphased="${meth_read_phased_dir}/${uid}.GRCh38.haplotagged.combined.bed.gz" # bed file from aligned_bam_to_cpg_scores (pooling all reads, irrespective of haplotype)
 
 	# OUTPUT FILES 
 	bed_meth_founder_phased_all_cpgs="${output_dir}/${uid}.dna-methylation.founder-phased.all_cpgs.bed"
 
     nohup python src/expand_to_all_cpgs.py \
 		--bed_all_cpgs ${bed_all_cpgs} \
-		--bed_hap_map ${bed_hap_map} \
         --bed_meth_founder_phased ${bed_meth_founder_phased} \
+        --bed_meth_unphased ${bed_meth_unphased} \
+		--pb_cpg_tool_mode ${pb_cpg_tool_mode} \
         --bed_meth_founder_phased_all_cpgs ${bed_meth_founder_phased_all_cpgs} \
         > ${output_dir}/${uid}.log 2>&1 & 
 
--- src/expand_to_all_cpgs.py
+++ src/expand_to_all_cpgs.py
@@ -1,11 +1,12 @@
 import argparse
 import logging
 import polars as pl 
-import bioframe as bf
 from pathlib import Path
 
 from read_data import read_data
 from write_data import write_bed
+from get_meth_hap1_hap2 import read_meth_level
+from version_sort import version_sort
 
 def read_all_cpgs(bed_all_cpgs): 
     return pl.read_csv(
@@ -31,49 +32,39 @@ def write_bed_and_header(file_path, df):
     assert file_suffix == ".bed"
     write_bed(parent_dir, df, file_stem)
 
-def read_hap_map_blocks(bed_hap_map): 
-    return (
-        read_bed_and_header(bed_hap_map)
-        .drop([
-            'paternal_haplotype',
-            'maternal_haplotype',
-            'haplotype_concordance',
-            'num_het_SNVs'
-        ])
-    )   
-
-def assign_hap_map_blocks_to_cpgs(df_all_cpgs, df_hap_map_blocks): 
-    return (
-        pl
-        .from_pandas(bf.overlap(
-            df_all_cpgs.to_pandas(), 
-            df_hap_map_blocks.to_pandas(), 
-            how='left',
-            suffixes=('', '_hap_map_block')
-        ))
-        .drop([
-            'chrom_hap_map_block'
-        ])
-    )
-
 def read_meth_founder_phased(bed_meth_founder_phased): 
     return read_bed_and_header(bed_meth_founder_phased)
 
-def expand_meth_to_all_cpgs(df_all_cpgs_with_hap_map_blocks, df_meth_founder_phased, bed_meth_founder_phased_all_cpgs): 
+def read_meth_unphased(bed_meth_unphased, pb_cpg_tool_mode): 
     return (
-        df_all_cpgs_with_hap_map_blocks
+        read_meth_level(bed_meth_unphased, pb_cpg_tool_mode)
+        .rename({
+            'chromosome': 'chrom'
+        })
+    )
+
+def expand_meth_to_all_cpgs(df_all_cpgs, df_meth_founder_phased, df_meth_unphased): 
+    df = (
+        df_all_cpgs
         .join(
             df_meth_founder_phased,
-            on=df_all_cpgs_with_hap_map_blocks.columns, 
+            on=['chrom', 'start', 'end'],
+            how="left"
+        )
+        .join(
+            df_meth_unphased,
+            on=['chrom', 'start', 'end'],
             how="left"
         )
     )
+    return version_sort(df)
 
 def main(): 
     parser = argparse.ArgumentParser(description='Expand output of tapestry to include all CpG sites')
     parser.add_argument('--bed_all_cpgs', required=True, help='All CpG sites')
-    parser.add_argument('--bed_hap_map', required=True, help='Hap-map blocks')
     parser.add_argument('--bed_meth_founder_phased', required=True, help='Founder-phased methylation levels')
+    parser.add_argument('--bed_meth_unphased', required=True, help='Unphased methylation levels')
+    parser.add_argument('--pb_cpg_tool_mode', required=True, help='--pileup-mode argument of aligned_bam_to_cpg_scores')
     parser.add_argument('--bed_meth_founder_phased_all_cpgs', required=True, help='Founder-phased methylation levels at all CpG sites (including null methylation levels)')
     args = parser.parse_args()
 
@@ -85,19 +76,16 @@ def main():
     logger = logging.getLogger(__name__)
 
     df_all_cpgs = read_all_cpgs(args.bed_all_cpgs)
-    logger.info(f"Read all CpG sites from {args.bed_all_cpgs}.")
-
-    df_hap_map_blocks = read_hap_map_blocks(args.bed_hap_map)
-    logger.info(f"Read all hap-map blocks from {args.bed_hap_map}.")
-
-    df_all_cpgs_with_hap_map_blocks = assign_hap_map_blocks_to_cpgs(df_all_cpgs, df_hap_map_blocks)
-    logger.info(f"Each CpG site has been assigned an overlapping hap-map block, if such exists.")
+    logger.info(f"Read all CpG sites from {args.bed_all_cpgs}")
 
     df_meth_founder_phased = read_meth_founder_phased(args.bed_meth_founder_phased)
     logger.info(f"Read CpG sites at which methylation levels have been phased to founder haplotypes")
 
-    df_meth_founder_phased_all_cpgs = expand_meth_to_all_cpgs(df_all_cpgs_with_hap_map_blocks, df_meth_founder_phased, args.bed_meth_founder_phased_all_cpgs)
-    logger.info(f"Expanded DNA methylation to all CpG sites.")
+    df_meth_unphased = read_meth_unphased(args.bed_meth_unphased, args.pb_cpg_tool_mode) 
+    logger.info(f"Read CpG sites at which unphased methylation levels are available")
+
+    df_meth_founder_phased_all_cpgs = expand_meth_to_all_cpgs(df_all_cpgs, df_meth_founder_phased, df_meth_unphased)
+    logger.info(f"Expanded DNA methylation to include all CpG sites, and unphased methylation levels (where available)")
 
     write_bed_and_header(args.bed_meth_founder_phased_all_cpgs, df_meth_founder_phased_all_cpgs)
     logger.info(f"Wrote expanded methylation dataframe to {args.bed_meth_founder_phased_all_cpgs}")
