a4fcccbc511d42b287681482a8ae388cf42800f0
petermchale <peter.thomas.mchale@gmail.com>
2025-10-17 20:19:50 -0600

    add general-purpose function for computing methylation from tapestry output

---
+++ src/util/compute_methylation.py
@@ -0,0 +1,76 @@
+import bioframe as bf # https://bioframe.readthedocs.io/en/latest/index.html
+import polars as pl
+
+from version_sort import version_sort
+
+def generate_methylation_expressions():
+    """
+    Generates a generalized list of Polars expressions for aggregating methylation data.
+
+    This function creates expressions to calculate the mean and count for various
+    methylation level columns, looping through different alleles ('pat', 'mat')
+    and level types ('count', 'model').
+
+    Returns:
+        A list of Polars expressions that can be used in an .agg() clause.
+    """
+    expressions = [
+        pl.len().alias("num_cpgs"),
+    ]
+
+    level_types = ['count', 'model']
+    alleles = ['pat', 'mat']
+
+    for allele in alleles: 
+        expression = pl.col(f"founder_haplotype_{allele}").unique()
+        expressions.append(expression)
+    
+    for level_type in level_types:
+        column_name = f"methylation_level_{level_type}"
+        expressions.extend([
+            # .mean() works by summing all the non-null values and dividing by the count of those non-null values
+            pl.col(column_name).mean().alias(f"mean_of_non_null_{level_type}_based_meths"), 
+            pl.col(column_name).count().alias(f"num_cpgs_with_non_null_{level_type}_based_meth"),
+        ])
+
+    for allele in alleles:
+        for level_type in level_types:
+            expression = pl.col(f"methylation_level_{allele}_{level_type}").mean().alias(f"mean_of_non_null_{level_type}_based_meths_on_{allele}_haplotype")
+            expressions.append(expression)
+            
+    return expressions
+
+def compute_methylation(df_intervals, df_meth, aggregation_expressions=generate_methylation_expressions()): 
+    assert df_intervals.columns == ['chrom', 'start', 'end']
+
+    df_intersected = bf.overlap(
+        df_intervals.to_pandas(),
+        df_meth.to_pandas(),
+        how='inner', # "inner" is sufficient when df_intervals covers the genome 
+        suffixes=('_intervals', ''),
+        return_overlap=False
+    )
+    df_intersected = pl.from_pandas(df_intersected)
+    
+    return version_sort(
+        df_intersected
+        .group_by(["chrom_intervals", "start_intervals", "end_intervals"])
+        .agg(aggregation_expressions)
+        .rename({
+            "chrom_intervals": "chrom", 
+            "start_intervals": "start", 
+            "end_intervals": "end"
+        }) 
+    )
+
+def test_polars_expressions(): 
+    # Generate the list of expressions by calling the function.
+    aggregation_expressions = generate_methylation_expressions()
+
+    # Print the generated expressions to see what they look like.
+    print("--- Generated Polars Expressions ---")
+    for expr in aggregation_expressions:
+        print(expr)
+
+if __name__ == '__main__':
+    test_polars_expressions()
