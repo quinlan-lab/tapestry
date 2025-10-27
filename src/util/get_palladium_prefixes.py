f4bf38ebf21c6afc243c615f29ffe5df48c6fdea
petermchale <peter.thomas.mchale@gmail.com>
2025-10-27 12:30:48 -0600

    generalize get_palladium_prefixes.py

--- src/util/get_palladium_prefixes.py
+++ src/util/get_palladium_prefixes.py
@@ -3,8 +3,15 @@ from get_id_to_paths import (
     get_prefixes
 )
 
-uid_to_path = get_uid_to_path__epic_and_hifi()
-prefixes = get_prefixes(uid_to_path)
+def get_prefixes_wrapper():
+    uid_to_path = get_uid_to_path__epic_and_hifi()
+    prefixes = get_prefixes(uid_to_path)
+    return prefixes
 
-for prefix in prefixes:
-    print(prefix)
+def main(): 
+    prefixes = get_prefixes_wrapper()
+    for prefix in prefixes:
+        print(prefix)
+
+if __name__ == '__main__': 
+    main()
