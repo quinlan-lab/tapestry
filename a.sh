1b0399393b504a358edccb6f856d36b81478010a
petermchale <peter.thomas.mchale@gmail.com>
2025-12-02 18:00:00 -0700

    draft showing how SNVs are missed in sample-specific vcfs and even joint-called vcfs

--- a.sh
+++ a.sh
@@ -1,5 +1,4 @@
 # TODO 
-# 1. 
 
 bcftools query -r chr1:146169000-146171000 -s 200081 -f '[%CHROM\t%POS\t%SAMPLE\t%REF\t%ALT\t%GT\n]' /scratch/ucgd/lustre-labs/quinlan/data-shared/datasets/Palladium/TRGT/from_aws/GRCh38_v1.0_50bp_merge/493ef25/hiphase/200081.GRCh38.deepvariant.glnexus.phased.vcf.gz
 bcftools query -r chr1:146169000-146171000 -s 200081 -f '[%CHROM\t%POS\t%SAMPLE\t%REF\t%ALT\t%GT\n]' /scratch/ucgd/lustre-labs/quinlan/data-shared/read-backed-phasing/200081.GRCh38.deepvariant.glnexus.phased.vcf.gz
