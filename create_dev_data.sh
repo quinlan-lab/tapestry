7d79ee83551c0ecd889e74f03282c1b52f3e4cfe
petermchale <peter.thomas.mchale@gmail.com>
2026-03-10 14:40:56 -0600

    change dev region to one with more normal coverage

--- create_dev_data.sh
+++ create_dev_data.sh
@@ -18,7 +18,7 @@ kid_id="NA12878"
 dad_id="NA12891" 
 mom_id="NA12892"
 
-DEV_REGION="chr22:20000000-21000000"
+DEV_REGION="chr1:1000000-2000000"
 
 echo "Creating dev data in ${DEV_DIR} for region ${DEV_REGION}..."
 
